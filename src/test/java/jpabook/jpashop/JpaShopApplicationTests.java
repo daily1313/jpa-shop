435644dfba68f3740bb3e51e87bcb04d72433544
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2023-02-15 00:02:17 +0900

    Initial commit

---
+++ src/test/java/jpabook/jpashop/JpaShopApplicationTests.java
@@ -0,0 +1,14 @@
+package jpabook.jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class JpaShopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
