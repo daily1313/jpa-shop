f817b462d26957b81486bba2f27552d50003e6bd
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2023-02-20 02:11:56 +0900

    Refactor : MemberRepository 클래스 관련 메서드 수정 및 기능 추가

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.repository;
 
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import jpabook.jpashop.domain.member.Member;
@@ -14,13 +15,30 @@ public class MemberRepository {
     //@PersistenceContext 어노테이션이 있을 경우
 
     //Cmd + Shift + T (Test 코드)
-    public Long save(Member member) {
+    public void save(Member member) {
         em.persist(member);
-        return member.getId();
     }
 
-    public Member find(Long id) {
+    public Member findOne(Long id) {
         return em.find(Member.class, id);
     }
+
+    public List<Member> findAll() {
+
+        // SQL : 테이블 대상으로 쿼리
+        // JPQL : 엔티티 객체를 대상으로 쿼리
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name" , name)
+                .getResultList();
+    }
+
+
+
 
 }
