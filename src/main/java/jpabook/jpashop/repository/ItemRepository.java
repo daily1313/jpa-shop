f013daaa17c5a8df0c90dbbce781c9cd04fadd0b
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2023-02-25 22:27:34 +0900

    feat : ItemRepository.class

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.repository;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if(item.getId() == null) {
+            em.persist(item);
+            // item 처음 생성 할 때(id 값이 없을 때, 영속성 컨텍스트에 저장)
+        } else {
+            em.merge(item);
+            // 그렇지 않은 경우 (update)
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+}
