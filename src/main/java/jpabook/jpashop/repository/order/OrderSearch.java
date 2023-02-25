fee0a3fbc9c3e03bb581fc5e365dbfff1a9cdb1a
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2023-02-26 03:59:08 +0900

    feat : 주문 검색 기능 추가

---
+++ src/main/java/jpabook/jpashop/repository/order/OrderSearch.java
@@ -0,0 +1,12 @@
+package jpabook.jpashop.repository.order;
+
+import jpabook.jpashop.domain.order.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class OrderSearch {
+    private String memberName; // 회원 이름
+    private OrderStatus orderStatus; // 주문 상태[ORDER, CANCEL]
+
+}
