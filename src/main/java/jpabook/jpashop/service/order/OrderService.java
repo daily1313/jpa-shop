b186d0ab267784a7277f9afa279b367151b8f8fe
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2023-02-26 03:04:22 +0900

    feat : Add OrderService

---
+++ src/main/java/jpabook/jpashop/service/order/OrderService.java
@@ -0,0 +1,65 @@
+package jpabook.jpashop.service.order;
+
+
+import java.util.List;
+import jpabook.jpashop.domain.delivery.Delivery;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.domain.member.Member;
+import jpabook.jpashop.domain.order.Order;
+import jpabook.jpashop.domain.orderItem.OrderItem;
+import jpabook.jpashop.repository.item.ItemRepository;
+import jpabook.jpashop.repository.member.MemberRepository;
+import jpabook.jpashop.repository.order.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    //주문
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+    /**
+     * 주문
+     */
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+
+        //엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        //배송정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+        //주문 상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        //주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        //주문 저장
+        orderRepository.save(order);
+
+        return order.getId();
+    }
+    // 주문 취소
+    @Transactional
+    public void cancelOrder(Long orderId) {
+        // 주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+        // 주문 취소
+        order.cancel();
+    }
+
+//    //검색
+//    public List<Order> findOrders(OrderSerach orderSerach) {
+//        return orderRepository.findAll(orderSerach);
+//    }
+}
