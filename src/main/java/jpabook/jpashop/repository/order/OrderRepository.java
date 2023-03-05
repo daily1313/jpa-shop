ba65734a2d31ade260e47f514c40373c7b93dd96
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2023-03-05 19:24:11 +0900

    feat : JPQL(엔티티 조회 쿼리문) 추가 (페치 조인 최적화)

--- src/main/java/jpabook/jpashop/repository/order/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/OrderRepository.java
@@ -65,12 +65,14 @@ public class OrderRepository {
                 .getResultList();
     }
 
-    public List<OrderSimpleQueryDto> findOrderDtos() {
-       return em.createQuery(
-                "select new jpabook.jpashop.repository.order.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.status, d.address) " +
-                        " from Order o" +
-                        " join o.member m" +
-                        " join o.delivery d", OrderSimpleQueryDto.class)
+
+    public List<Order> findAllWithItem() {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d" +
+                        " join fetch o.orderItems oi" +
+                        " join fetch oi.item", Order.class)
                 .getResultList();
     }
 }
